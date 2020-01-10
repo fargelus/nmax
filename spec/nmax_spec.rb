3bd1555fd35bc3d4749b2d1d98b1980db03d4f48
fargelus <ddraudred@gmail.com>
2020-01-10 14:25:58 +0300

    Exec file changed

--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Nmax do
     expect(Nmax::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'prints' do
+    Nmax::Scanner.call(1)
   end
 end
